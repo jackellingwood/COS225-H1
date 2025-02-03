d0bd3aeaa7a5ca3acdf586a599f15af5e181adc6
jackellingwood <ellingwoodjack@gmail.com>
2025-02-03 15:27:16 -0500

    ParkingTester added and shows the correct output!

--- src/ParkingTester.java
+++ src/ParkingTester.java
@@ -1,4 +1,24 @@
 //TODO fill in with other classes
 public class ParkingTester {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        ParkingLot lot = new ParkingLot();
+
+        System.out.println(lot.toString());
+
+        Car carA = new Car("Infiniti", "Blue", true);
+
+        lot.parkCar(carA);
+
+        System.out.println(lot.toString());
+
+        Car carB = new Car("Cadillac", "Black", false);
+
+        lot.parkCar(carB);
+
+        System.out.println(lot.toString());
+
+        lot.unparkCar(0);
+
+        System.out.println(lot.toString());
+    }
 }

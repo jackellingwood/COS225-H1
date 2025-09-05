0a9c4de6fe9552a7261ee501ca99be903cf8f888
jackellingwood <ellingwoodjack@gmail.com>
2025-09-05 10:13:52 -0400

    Update ParkingTester.java

--- src/ParkingTester.java
+++ src/ParkingTester.java
@@ -2,22 +2,22 @@ public class ParkingTester {
     public static void main(String[] args) {
         ParkingLot lot = new ParkingLot();
 
-        System.out.println(lot.toString());
+        System.out.println(lot);
 
         Car carA = new Car("Infiniti", "Blue", true);
 
         lot.parkCar(carA);
 
-        System.out.println(lot.toString());
+        System.out.println(lot);
 
         Car carB = new Car("Cadillac", "Black", false);
 
         lot.parkCar(carB);
 
-        System.out.println(lot.toString());
+        System.out.println(lot);
 
         lot.unparkCar(0);
 
-        System.out.println(lot.toString());
+        System.out.println(lot);
     }
 }
